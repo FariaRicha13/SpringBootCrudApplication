3831ccbe64bc9d1a62f635d47ce0ea64573a258f
FariaRicha13 <fariahricha13@gmail.com>
2023-03-28 11:41:38 +0600

    First commit on 28th March 2023

---
+++ src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './Layout/Navbar';
+import Home from './Pages/Home'
+import AddUser from './Users/AddUser'
+import EditUser from './Users/EditUser';
+import ViewUser from './Users/ViewUser';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/adduser" element={<AddUser/>}/>
+          <Route exact path="/edituser/:id" element={<EditUser/>}/>
+          <Route exact path="/viewuser/:id" element={<ViewUser/>}/>
+        </Routes>
+      
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
